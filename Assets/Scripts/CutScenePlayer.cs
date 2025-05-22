2c290451571bc1bdc41cd593915053a07d659da9
HyunL <sirah31@ajou.ac.kr>
2025-05-22 18:00:07 +0900

    Title & Cutscene 수정

--- Assets/Scripts/CutScenePlayer.cs
+++ Assets/Scripts/CutScenePlayer.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class CutScenePlayer : MonoBehaviour
 {
+    // 인스펙터에서 설정 가능하도록 직렬화
     [SerializeField] private Image backgroundA;
     [SerializeField] private Image backgroundB;
     [SerializeField] private TextMeshProUGUI subtitleText;
@@ -14,19 +15,19 @@ public class CutScenePlayer : MonoBehaviour
     [SerializeField] private List<Sprite> backgrounds;
     [SerializeField] private List<string> subtitles;
 
+    // Coroutine 시작
     public void StartCutscene()
     {
         StartCoroutine(PlayCutscene());
+        StartCoroutine(PlaySubtitle());
     }
 
+    // 배경 재생 (코루틴)
     IEnumerator PlayCutscene()
     {
         backgroundA.sprite = backgrounds[0];
-        backgroundA.color = Color.white;
-        backgroundB.color = new Color(1, 1, 1, 0); // 투명
-
-        subtitleText.text = "";
-        yield return StartCoroutine(ShowSubtitle(subtitles[0]));
+        backgroundA.color = new Color(0, 0, 0, 1);
+        backgroundB.color = new Color(1, 1, 1, 0);
 
         for (int i = 1; i < backgrounds.Count; i++)
         {
@@ -35,6 +36,7 @@ public class CutScenePlayer : MonoBehaviour
 
             // 알파값 서서히 조절 (배경 페이드)
             float t = 0;
+
             while (t < fadeDuration)
             {
                 t += Time.deltaTime;
@@ -45,29 +47,37 @@ public class CutScenePlayer : MonoBehaviour
                 yield return null;
             }
 
-            // A와 B 스왑
+            // A와 B Swap
             var temp = backgroundA;
             backgroundA = backgroundB;
             backgroundB = temp;
 
-            backgroundB.color = new Color(1, 1, 1, 0); // 다시 투명
+            backgroundB.color = new Color(1, 1, 1, 0);
 
-            // 자막 갱신
-            subtitleText.text = "";
-            yield return StartCoroutine(ShowSubtitle(subtitles[i]));
+            yield return new WaitForSeconds(2.5f); // 배경 유지 시간
         }
-
-        // 컷씬 끝나면 다음 씬으로 넘어가기 등
     }
 
+    // 자막 재생 (코루틴)
+    IEnumerator PlaySubtitle()
+    {
+        for (int i = 0; i < subtitles.Count; i++)
+        {
+            yield return StartCoroutine(ShowSubtitle(subtitles[i]));
+        }
+    }
+
+    // 자막 타이핑 효과 (코루틴)
     IEnumerator ShowSubtitle(string sentence)
     {
         subtitleText.text = "";
+
         foreach (char c in sentence)
         {
             subtitleText.text += c;
             yield return new WaitForSeconds(0.05f); // 속도 조절 가능
         }
+
         yield return new WaitForSeconds(1f); // 자막 유지 시간
     }
 }
